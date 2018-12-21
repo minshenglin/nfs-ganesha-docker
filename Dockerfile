dc50949c951186f2a4edfe260bab059531890101
Vincent <minsheng.l@inwinstack.com>
2018-12-21 01:45:39 +0000

    using ganesha config in ceph pool instead of local config

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM centos:7
 
 COPY ceph.repo /etc/yum.repos.d/ceph.repo
 COPY nfs.repo /etc/yum.repos.d/nfs.repo
-COPY ganesha.conf /etc/ganesha/ganesha.conf
+
 RUN yum install -y epel-release
 RUN rpm --import 'https://download.ceph.com/keys/release.asc'
+RUN yum install nfs-ganesha-ceph nfs-ganesha-rgw -y
+
 EXPOSE 2049
-RUN yum install nfs-ganesha-rgw -y
+
 COPY entrypoint.sh /tmp/entrypoint.sh
 ENTRYPOINT "/tmp/entrypoint.sh"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
+config_path="/etc/ganesha/ganesha.conf"
+
+# check ganesha config variable
+if [[ -z "${GANESHA_CONFIG_POOL}" || -z "${GANESHA_CONFIG}" ]]; then
+	echo "GANESHA_CONFIG_POOL and GANESHA_CONFIG is not set"
+	exit 1
+fi
+
+# start rpcbind
 /usr/sbin/rpcbind
-#rpc.statd -L
-#rpc.idmapd
-/usr/bin/ganesha.nfsd -F -f /etc/ganesha/ganesha.conf -L STDOUT
+/usr/sbin/rpc.statd -L
+/usr/sbin/rpc.idmapd
+
+mkdir -p /etc/ganesha
+
+echo "RADOS_URLS {
+	ceph_conf = /etc/ceph/ceph.conf;
+	userid = admin;
+}
+
+%url rados://$GANESHA_CONFIG_POOL/$GANESHA_CONFIG
+
+NFSv4 {
+	Delegations = false;
+        RecoveryBackend = rados_ng;
+        Minor_Versions =  1,2;
+}
+
+RGW {
+        ceph_conf = /etc/ceph/ceph.conf;
+        name = \"client.admin\";
+        cluster = ceph;
+        init_args = \"--keyring=/etc/ceph/ceph.client.admin.keyring\";
+}" > $config_path
+
+cat $config_path
+
+# start nfs-ganesha
+/usr/bin/ganesha.nfsd -F -f $config_path -L STDOUT
